f33b44b78b52ed935bbc25bcdbd1dc50c66798a8
Kim Yoo <kimyoo04eco@naver.com>
2022-09-07 00:16:48 +0900

    1.9 Websockets Conclusions

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,5 +1,6 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 
 // 브라우저가 서버로 연결하는 코드
 const socket = new WebSocket(`ws://${window.location.host}`);
@@ -11,7 +12,9 @@ socket.addEventListener("open", () => {
 
 // 서버에서 브라우저로 메시지를 보내면 작동
 socket.addEventListener("message", (message) => {
-  console.log("New message: ", message.data);
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 // 서버가 닫히면 작동
@@ -20,16 +23,39 @@ socket.addEventListener("close", () => {
 });
 
 function handleSubmit(event) {
-  // 새로고침 방지
-  event.preventDefault();
+  event.preventDefault(); // 새로고침 방지
   const input = messageForm.querySelector("input");
+  // 서버로 form에 입력했던 값(메시지)를 서버로 string으로 변환 후 전송
+  socket.send(makeMessage("new_message", input.value));
+  input.value = ""; // 입력 창 초기화
+}
 
-  // 서버로 form에 입력했던 값(메시지)를 전송
-  socket.send(input.value);
+function handleNickSubmit(event) {
+  event.preventDefault(); // 새로고침 방지
+  const input = nickForm.querySelector("input");
+  // 서버로 form에 입력했던 값(메시지)를 서버로 string으로 변환 후 전송
+  socket.send(makeMessage("nickname", input.value));
+  input.value = ""; // 입력 창 초기화
+}
 
-  // 입력 창 초기화
-  input.value = "";
+// json type ==> string
+function makeMessage(type, payload) {
+  const msg = {type: type, payload: payload};
+  return JSON.stringify(msg);
 }
 
 //form의 submit이 일어나면 함수 실행
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
+
+/*
+===================================== 작동 순서 =====================================
+1. form의 input에 메시지나 닉네임 입력 및 button 클릭
+2. addEventListener 작동 
+3. handleSubmit 작동
+4. socket.send를 통해 서버로 메시지 문자열 전송
+5. 서버가 메시지를 받았을 때 json 전환 및 type 별 작동 구분
+6. type이 new_message 일 때 다시 브라우저로 payload만 메시지 전송
+7. 브라우저는 메시지를 받은 것을 messageList에 li테그로 append시켜 화면에 출력
+====================================================================================
+*/
--- src/server.js
+++ src/server.js
@@ -11,18 +11,21 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
+// =========================================================================
 
 // http server
 const server = http.createServer(app);
 // wss server on top of http server
 const wss = new WebSocket.Server({server});
+// =========================================================================
 
 // 서버와 연결되는 브라우저들 담는 array
 const sockets = [];
 
 // after connection ==> 브라우저가 socket을 받는다.
 wss.on("connection", (socket) => {
-  sockets.push(socket);
+  sockets.push(socket); // 사이트를 방문한 브라우저들을 모으는 곳
+  socket["nickname"] = "Anon"; // 기본으로 nickname을 Anon 으로 제공
   // 아래의 코드들은 브라우저가 서버에 connection 되었을 때만 실행
   console.log("Connected to Browser ✅");
 
@@ -30,8 +33,23 @@ wss.on("connection", (socket) => {
   socket.on("close", () => console.log("Disconnected from the Browser ❌"));
 
   // 서버에서 프론트로 메시지 전송
-  socket.on("message", (message) => {
-    sockets.forEach((aSocket) => aSocket.send(message.toString()));
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+
+    switch (message.type) {
+      // type 이 new_message 일 때, 브라우저로 (별명: 메시지 내용) 메시지 전송
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+        break;
+      // type 이 nickname 일 때, socket에 nickname 키와 message.payload를 값으로 할당
+      case "nickname":
+        socket["nickname"] = message.payload;
+        break;
+      default:
+        console.log("does not match message.type");
+    }
   });
 });
 
