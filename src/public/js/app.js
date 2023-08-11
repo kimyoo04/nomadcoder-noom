bf21e6c5989e1eac29659eadeca98d53e5b0c789
Kim Yoo <kimyoo04eco@naver.com>
2023-08-11 23:38:51 +0900

    3.2 Camera Switch, 카메라 선택 옵션 기능

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,28 +1,68 @@
 const socket = io();
-
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 
 let myStream;
 let muted = false;
 let cameraOff = false;
 
-async function getMedia() {
+async function getCameras() {
   try {
-    myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    const currentCamera = myStream.getVideoTracks()[0];
+    cameras.forEach((camera) => {
+      // 카메라 선택 옵션 생성
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+
+      // 현재 카메라를 선택한 카메라로 설정
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+      }
+
+      // 카메라 선택 옵션 추가
+      camerasSelect.appendChild(option);
     });
-    myFace.srcObject = myStream;
   } catch (e) {
     console.log(e);
   }
 }
 
-getMedia();
+async function getMedia(deviceId) {
+  // 기본 카메라 설정
+  const initialConstrains = {
+    audio: true,
+    video: { facingMode: "user" },
+  };
+
+  // 선택한 카메라 설정
+  const cameraConstraints = {
+    audio: true,
+    video: { deviceId: { exact: deviceId } },
+  };
+
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstraints : initialConstrains
+    );
+
+    myFace.srcObject = myStream;
+
+    // 카메라를 선택하지 않았을 경우 카메라 선택 옵션 생성
+    if (!deviceId) await getCameras();
+  } catch (e) {
+    console.log(e);
+  }
+}
 
 function handleMuteClick() {
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
   if (!muted) {
     muteBtn.innerText = "Unmute";
     muted = true;
@@ -31,7 +71,11 @@ function handleMuteClick() {
     muted = false;
   }
 }
+
 function handleCameraClick() {
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
   if (cameraOff) {
     cameraBtn.innerText = "Turn Camera Off";
     cameraOff = false;
@@ -41,5 +85,13 @@ function handleCameraClick() {
   }
 }
 
+// 카메라 변경 이벤트
+async function handleCameraChange() {
+  await getMedia(camerasSelect.value);
+}
+
+getMedia();
+
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
+camerasSelect.addEventListener("input", handleCameraChange);
